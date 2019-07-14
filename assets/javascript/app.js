bcab8ea9da5d0922af81740387c18727946f03fe
Daryl Acut <darylacut@yahoo.com>
2019-07-14 18:53:46 -0400

    working on toggle show quiz

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,29 @@
-var points = 0;
-var rightAnswers = 0;
-var wrongAnswers = 0;
+function toggleQuiz() {
+    var showQuiz = document.getElementById("myQuiz");
+    var showStartbutton = document.getElementById("startButton");
+    if (showQuiz.style.display === "none") {
+        showQuiz.style.display = "block";
+
+    } else {
+        showQuiz.style.display = "none";
+    }
+}
 
 
-$("#endGame").on('click', function (checkAnswers);
+//var points = 0;
+//var rightAnswers = 0;
+//var wrongAnswers = 0;
 
-function checkAnswers () {
+
+//$("#endGame").on('click', function (checkAnswers);
+
+//function checkAnswers () {
     
-    if(document.getElementByValue('gender_Male').checked) {
+  //  if(document.getElementByValue('gender_Male').checked) {
         //Male radio button is checked
-      }else if(document.getElementById('gender_Female').checked) {
+  //    }else if(document.getElementById('gender_Female').checked) {
         //Female radio button is checked
 
-    for 
-    points++;
-}
+  //  for 
+  ///  points++;
+//}
