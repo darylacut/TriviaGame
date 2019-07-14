a13bcbe513baac7533e525848d309c4672736654
Daryl Acut <darylacut@yahoo.com>
2019-07-14 17:38:25 -0400

    added display none to hide elements

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+var points = 0;
+var rightAnswers = 0;
+var wrongAnswers = 0;
+
+
+$("#endGame").on('click', function (checkAnswers);
+
+function checkAnswers () {
+    
+    if(document.getElementByValue('gender_Male').checked) {
+        //Male radio button is checked
+      }else if(document.getElementById('gender_Female').checked) {
+        //Female radio button is checked
+
+    for 
+    points++;
+}
