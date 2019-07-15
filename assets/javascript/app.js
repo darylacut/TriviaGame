16f2789833ce03f35172c57812368ebdecbf6eed
Daryl Acut <darylacut@yahoo.com>
2019-07-15 02:11:24 -0400

    added if statements for questions to check if correct or not

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,14 @@
 
-
-   
+    
 function toggleQuiz() {
-    var correct = 0;
-    var incorrect = 0;
+    var correctCount = 0;
+    var incorrectCount = 0;
 
     var showQuiz = document.getElementById("myQuiz");
     var showStartbutton = document.getElementById("startButton");
     var showResults = document.getElementById("results");
 
-
+   
     if (showQuiz.style.display === "none") {
         showQuiz.style.display = "block";
         showStartbutton.style.display = "none";
@@ -18,35 +17,101 @@ function toggleQuiz() {
         showQuiz.style.display = "none";
     }
 
+
         setTimeout(sixtySeconds, 1000 * 10);
 
         function sixtySeconds() {
+
+            if (document.getElementById("deer").checked === true) {
+                correctCount++;
+            } else {
+                incorrectCount++;
+            }
+        
+            if (document.getElementById("Canada").checked === true) {
+                correctCount++;
+            } else {
+                incorrectCount++;
+            }
+        
+        
+            if (document.getElementById("J.K. Rowling").checked === true) {
+                correctCount++;
+            } else {
+                incorrectCount++;
+            }
+        
+        
+            if (document.getElementById("Hobbes").checked === true) {
+                correctCount++;
+            } else {
+                incorrectCount++;
+            }
+        
+        
+            if (document.getElementById("King of Hearts").checked === true) {
+                correctCount++;
+            } else {
+                incorrectCount++;
+            }
+
+
             showQuiz.style.display = "none";
             showResults.style.display = "block";
+
+            $('#correct').text(correctCount);
+            $('#incorrect').text(incorrectCount); 
         }   
+
 
 }
 
 
 function endGame () {
+    var correctCount = 0;
+    var incorrectCount = 0;
+
     var showQuiz = document.getElementById("myQuiz");
     var showStartbutton = document.getElementById("startButton");
     var showResults = document.getElementById("results");
 
+    if (document.getElementById("deer").checked === true) {
+        correctCount++;
+    } else {
+        incorrectCount++;
+    }
+
+    if (document.getElementById("Canada").checked === true) {
+        correctCount++;
+    } else {
+        incorrectCount++;
+    }
+
+
+    if (document.getElementById("J.K. Rowling").checked === true) {
+        correctCount++;
+    } else {
+        incorrectCount++;
+    }
+
+
+    if (document.getElementById("Hobbes").checked === true) {
+        correctCount++;
+    } else {
+        incorrectCount++;
+    }
+
+
+    if (document.getElementById("King of Hearts").checked === true) {
+        correctCount++;
+    } else {
+        incorrectCount++;
+    }
+
+    $('#correct').text(correctCount);
+    $('#incorrect').text(incorrectCount); 
 
     showQuiz.style.display = "none";
     showResults.style.display = "block";
 }
 
-
-
-// {
-    
-  //  if(document.getElementByValue('gender_Male').checked) {
-        //Male radio button is checked
-  //    }else if(document.getElementById('gender_Female').checked) {
-        //Female radio button is checked
-
-  //  for 
-  ///  points++;
-//}
